de2177d25ac5702323ba4d5dc5ac35b524c2aa82
Henrik <han@rts.dk>
2025-06-04 13:09:10 +0200

    Refactor project structure and update assets
    - Added new CSS file for styling the search bar and game layout.
    - Removed obsolete JavaScript file related to the search functionality.
    - Updated HTML file to reference new JavaScript and CSS assets.
    - Set homepage in package.json for deployment.
    - Adjusted Vite configuration for better asset handling and build output.

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import pagesPlugin from 'vite-plugin-pages'
 
-export default defineConfig(({ mode }) => ({
-  plugins: [react(), pagesPlugin({
-    importPath: 'absolute'
-  })],
-  base: mode === 'development' ? '/' : '/vejr-app-HenrikGlinka/',
+export default defineConfig(() => ({
+  plugins: [react(), pagesPlugin()],
+  base: './',
   build: {
     outDir: 'docs',
     emptyOutDir: true,
